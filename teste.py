5e97906073a78ae48638f4eabcba117c0cb05f81
Laiane-Nasci <80715768+Laiane-Nasci@users.noreply.github.com>
2023-05-03 21:35:37 -0300

    alteração na saudação

--- teste.py
+++ teste.py
@@ -1,4 +1,4 @@
 nome = input("Digite seu nome: ")
 print("Boa noite")
-print("Seja Bem Vindo, " + nome)
+print("Seja Muito Bem Vindo, " + nome)
 print("Obrigada por visitar por visitar nosso site.")
